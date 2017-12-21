a1edacadc46ee4a1f0e0c106d8f013d8e0f18296
flyingrub <flyingrub@protonmail.com>
2017-12-21 14:50:53 +0100

    improve color

--- js/visual.js
+++ js/visual.js
@@ -69,13 +69,9 @@ function drawFFT() {
         let mid = audio.midTimeline[index];
         let high = audio.highTimeline[index];
         let max = Math.max(bass, kick, mid, high);
-        let r, g, b;
-        let hue = bass / max + mid / max + kick / max + high/max;
-        hue/=4
-        let saturation = 0.9;
-        let value = 1;
-        let color = hsvToRgb(hue, saturation, value)
-        r = color[0]; g = color[1]; b=color[2];
+        let r = bass / max * 255;
+        let g = mid / max * 255;
+        let b = high / max * 255 *3;
         return 'rgb('+r+','+g+','+b+')';
     }
 
@@ -131,34 +127,3 @@ function drawFFT() {
 
     window.requestAnimationFrame(drawFFT);
 }
-/**
- * Converts an HSV color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
- * Assumes h, s, and v are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  v       The value
- * @return  Array           The RGB representation
- */
-function hsvToRgb(h, s, v) {
-    var r, g, b;
-
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
-
-    switch (i % 6) {
-      case 0: r = v, g = t, b = p; break;
-      case 1: r = q, g = v, b = p; break;
-      case 2: r = p, g = v, b = t; break;
-      case 3: r = p, g = q, b = v; break;
-      case 4: r = t, g = p, b = v; break;
-      case 5: r = v, g = p, b = q; break;
-    }
-
-    return [ r * 255, g * 255, b * 255 ];
-  }
